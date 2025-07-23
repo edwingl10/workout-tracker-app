3e6ae55ad88272913c9141b183fd73bd3d2997f9
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2025-07-23 14:55:59 -0700

    updated eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,8 +17,27 @@ const eslintConfig = [
     'plugin:jsx-a11y/recommended',
   ),
   {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['**/node_modules/**', '**/.next/**', '**/dist/**'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        process: 'readonly',
+        document: 'readonly',
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
     rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-one-expression-per-line': 'off',
       'react/jsx-props-no-spreading': 'off',
+      'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
     },
   },
 ];
